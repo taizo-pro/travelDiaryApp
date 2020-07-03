198080679b929ea0f4e1186e65a3b619fd32e354
taizo-pro <t2s.single@gmail.com>
2020-07-04 08:50:36 +0900

    アプリ内に情報を保存し、Welcome画面を表示させない

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, Platform, Image, StatusBar} from 'react-native';
+import {StyleSheet, Text, View, Platform, Image, StatusBar,} from 'react-native';
 import {
   createAppContainer,
   createBottomTabNavigator,
